6d6a6fe99e3d1d206135d4dc26dd2b619c6312ca
kkawakami08 <k.kawakami08@gmail.com>
2022-04-04 15:41:33 -0700

    addInfo button works with correct input values

--- javascript.js
+++ javascript.js
@@ -20,3 +20,25 @@ exitBtn.addEventListener("click", (event) => {
   promptWindow.style.visibility = "hidden";
   promptWindow.style.opacity = "0";
 });
+
+let titleInput = document.getElementById("title");
+let authorInput = document.getElementById("author");
+let pagesInput = document.getElementById("pages");
+let readInput = document.getElementById("read");
+
+let title;
+let author;
+let pages;
+let read;
+
+let addInfoBtn = document.getElementById("addInfo");
+addInfoBtn.addEventListener("click", (event) => {
+  title = titleInput.value;
+  author = authorInput.value;
+  pages = pagesInput.value;
+  read = readInput.checked;
+  console.log(title);
+  console.log(author);
+  console.log(pages);
+  console.log(read);
+});
