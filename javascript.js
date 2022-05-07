89bfcdfd59813a932768e131b33578f79544201d
kkawakami08 <k.kawakami08@gmail.com>
2022-05-07 09:57:19 -0700

    basic addBook function

--- javascript.js
+++ javascript.js
@@ -1,57 +1,24 @@
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+const permBook = new Book("The Hobbit", "J.R.R. Tolkien", 295);
+
 let myLibrary = [];
+myLibrary.push(permBook);
 
-function bookCard() {}
-
-let main = document.getElementById("main");
-
-myLibrary[0] = new Book("Harry Potter", "JK Rowling", 256, true);
-let card = document.createElement("div");
-main.appendChild(card);
-
-console.log(myLibrary[0]);
-
-function Book(title, author, pages, read) {
-  (this.title = title),
-    (this.author = author),
-    (this.pages = pages),
-    (this.read = read);
-}
-
-let promptWindow = document.getElementById("promptWindow");
-
-let addBtn = document.getElementById("add");
-addBtn.addEventListener("click", (event) => {
-  promptWindow.style.visibility = "visible";
-  promptWindow.style.opacity = "1";
-});
-
-let exitBtn = document.getElementById("exit");
-exitBtn.addEventListener("click", exit);
-
-function exit(e) {
-  promptWindow.style.visibility = "hidden";
-  promptWindow.style.opacity = "0";
-}
-
-let titleInput = document.getElementById("title");
-let authorInput = document.getElementById("author");
-let pagesInput = document.getElementById("pages");
-let readInput = document.getElementById("read");
-
-let title;
-let author;
-let pages;
-let read;
-let book;
-
-let addInfoBtn = document.getElementById("addInfo");
-addInfoBtn.addEventListener("click", (event) => {
-  title = titleInput.value;
-  author = authorInput.value;
-  pages = pagesInput.value;
-  read = readInput.checked;
-  book = new Book(title, author, pages, read);
+function addBook() {
+  let userTitle = prompt("Enter book title");
+  let userAuthor = prompt("Enter book's author");
+  let userPages = prompt("How many pages?");
+  console.log(
+    `You entered ${userTitle} by ${userAuthor} with ${userPages} pages`
+  );
+  const book = new Book(userTitle, userAuthor, userPages);
   myLibrary.push(book);
-  exit();
   console.log(myLibrary);
-});
+}
+
+addBook();
